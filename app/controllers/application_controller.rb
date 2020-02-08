30cc30985be7c5274a674a5a8ac9d924b8958f55
Kevin Hayden <kevin.fhayden@gmail.com>
2020-02-08 01:40:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,31 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    end
+    
+    erb :'/users/create_user'
+  end
+  
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to 'signup'
+    end
+    
+    user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect to '/tweets'
+  end
+  
+  get '/tweets' do
+
+  end
 
 end
